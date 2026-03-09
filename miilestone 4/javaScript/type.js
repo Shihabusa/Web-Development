113f1a04442846e191187e0240f96f686a94dc8b
Abdur Rahaman <ieltsshihab01@gmail.com>
2026-03-09 15:35:27 +0600

    tanks

---
+++ miilestone 4/javaScript/type.js
@@ -0,0 +1,8 @@
+var address=" sonir akra dania dhaka";
+var name="shihab";
+console.log(name);
+console.log(address);
+var passed=true;
+console.log(passed);
+console.log(typeof passed);
+console.log(typeof name);
---
+++ miilestone 4/javaScript/variable.js
@@ -0,0 +1,7 @@
+var weight = 38;
+var price =38;
+var year =2003;
+var money= 1230;
+var age;
+console.log(age)
+console.log(price)
